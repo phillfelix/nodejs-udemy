f484bd563e2e4a6f3cf7c12eae960f11c74bec65
Philipe Felix <philipe.felixdossantos@gmail.com>
2018-09-01 10:34:21 -0300

    Adicionando middlewares e log em arquivo

--- section-4/vidly/index.js
+++ section-4/vidly/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 const genres = require('./genres');
+const logger = require('./logger');
+const auth = require('./auth');
 const app = express();
 
-app.use(express.json());
+// third party middlewares
+app.use(helmet());
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), { flags: 'a' });
+app.use(morgan('combined', { stream: accessLogStream }));
 
+// built in middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// custom middlewares
+app.use(logger);
+app.use(auth);
+
+// custom router
 app.use(genres);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // get port from environment
 app.listen(port, () => console.log(`Vidly API listening on port ${port}`));
