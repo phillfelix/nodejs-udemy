740101ac21f8478fc072d32988b47b44d6044a8d
Philipe Felix <philipe.felixdossantos@gmail.com>
2018-09-01 10:53:51 -0300

    Logger em módulo externo

--- section-4/vidly/index.js
+++ section-4/vidly/index.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
-const morgan = require('morgan');
-const fs = require('fs');
-const path = require('path');
 const genres = require('./genres');
-const logger = require('./logger');
 const auth = require('./auth');
+const logger = require('./logger');
 const app = express();
 
 // third party middlewares
 app.use(helmet());
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), { flags: 'a' });
-app.use(morgan('combined', { stream: accessLogStream }));
 
 // built in middlewares
 app.use(express.json());
@@ -19,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // custom middlewares
-app.use(logger);
+app.use(logger(app.get('env')));
 app.use(auth);
 
 // custom router
--- section-4/vidly/logger.js
+++ section-4/vidly/logger.js
@@ -1,6 +1,22 @@
-function log(req, res, next) {
-  console.log('Logging...');
-  next();
+
+const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
+
+function logger(env) {
+  let logger;
+  if(env === 'development') {
+    logger = morgan('tiny')
+    console.log('Log enabled for all requests');
+  } else {
+    const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), { flags: 'a' });
+    logger = morgan('combined', {
+      stream: accessLogStream,
+      skip: (req, res) => res.statusCode < 400
+    });
+    console.log('Logging 4xx to file');
+  }
+  return logger;
 }
 
-module.exports = log;
+module.exports = logger;
